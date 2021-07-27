de5d3ae13b2bcc331df7c30031913488b72f8466
jakubnecessary <cabbage1991@gmail.com>
2021-07-27 19:39:03 +0100

    8tests passing

--- react_calculator/src/tests/Calculator.test.js
+++ react_calculator/src/tests/Calculator.test.js
@@ -68,6 +68,26 @@ describe('Calculator', () => {
     equals.simulate('click');
     expect(runningTotal.text()).toEqual('3');
   })
+  it('should concatenate multiple number button clicks', () => {
+    const button5 = container.find('#number5');
+    const button2 = container.find('#number2');
+    const button4 = container.find('#number4');
+    const add = container.find('#operator_add');
+    const multiply = container.find('#operator-multiply');
+    const divide = container.find('#operator-divide');
+    const equals = container.find('#operator-equals');
+    const runningTotal = container.find('#running-total');
+    button5.simulate('click');
+    add.simulate('click');
+    button5.simulate('click');
+    divide.simulate('click');
+    button2.simulate('click');
+    multiply.simulate('click');
+    button2.simulate('click');
+    equals.simulate('click');
+    expect(runningTotal.text()).toEqual('10');
+
+  })
 })
   
 
