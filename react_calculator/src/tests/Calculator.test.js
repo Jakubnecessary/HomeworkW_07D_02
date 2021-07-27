95f189776a850a8dabb0a261481de74a3ae24db5
jakubnecessary <cabbage1991@gmail.com>
2021-07-27 18:58:47 +0100

    2 tests passing

---
+++ react_calculator/src/tests/Calculator.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Calculator from '../containers/Calculator';
+import {mount, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('Calculator', () => {
+  let container;
+  beforeEach(() => {
+    container = mount(<Calculator/>)
+  })
+
+  it('should change running total on number enter', () => {
+    const button4 = container.find('#number4');
+    const runningTotal = container.find('#running-total');
+    button4.simulate('click');
+    expect(runningTotal.text()).toEqual('4');
+  })
+  it('should add 1 + 4 and get 5', () => {
+    const button1 = container.find('#number1');
+    const button4 = container.find('#number4');
+    const add = container.find('#operator_add');
+    const equals = container.find('#operator-equals');
+    const runningTotal = container.find('#running-total');
+    button1.simulate('click');
+    add.simulate('click');
+    button4.simulate('click');
+    equals.simulate('click');
+    expect(runningTotal.text()).toEqual('5');
+  })
+})
+
