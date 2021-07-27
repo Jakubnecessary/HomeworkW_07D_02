30d8f6347d4563c2b4d672854e11cce7b5e6810f
jakubnecessary <cabbage1991@gmail.com>
2021-07-27 20:04:51 +0100

    2/4 UI Integration tests passing

--- react_calculator/cypress/integration/calculator.spec.js
+++ react_calculator/cypress/integration/calculator.spec.js
@@ -7,4 +7,16 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+  it('should update the display of running total', () => {
+    cy.get('#number5').click();
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '52')
+  })
+  it('should update display after math', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '10')
+  })
 })
